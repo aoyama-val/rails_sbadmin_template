b0f6e15a4b65f75cb3ec531b434cd460f6ed1eba
Aoyama, Shotaro <aoyama@val.co.jp>
2016-05-16 12:53:03 +0900

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160514092443) do
 
-  create_table "customers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "name"
