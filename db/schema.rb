98238c6f536180ddb124a27046666b665b892e2e
Aoyama, Shotaro <aoyama@val.co.jp>
2016-05-17 14:16:42 +0900

    add unique to users.email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514092443) do
+ActiveRecord::Schema.define(version: 20160517051438) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(version: 20160514092443) do
     t.datetime "updated_at",      null: false
     t.datetime "last_login"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
